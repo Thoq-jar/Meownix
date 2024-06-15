c244ee97487a2f45c01b1ab3165f1cc6d683bf1e
Thoq <125930852+Thoq-jar@users.noreply.github.com>
2024-06-15 01:27:40 +0000

    Update install.sh

--- Meowtop/install.sh
+++ Meowtop/install.sh
@@ -1,11 +1,20 @@
-echo "Welcome to Meowtop"
+echo "Welcome to Meowtop DE"
+
+echo "Cleaning..."
+sudo rm -rf /usr/bin/Meowtop
+sudo rm -rf /usr/share/xsessions/Meowtop.desktop
+sudo rm -rf /usr/bin/Meowtop
+sudo rm -rf /usr/bin/meowtopde
+sudo rm -rf /usr/libexec/meowtop-binary
 
 echo "Installing files..."
 sudo mkdir /usr/bin/Meowtop
 sudo cp Meowtop.desktop /usr/share/xsessions/
-sudo cp Meowtop /usr/bin/Meowtop
-sudo cp meowtop /usr/bin/local
+sudo cp start-meowtop /usr/bin/Meowtop
+sudo cp meowtopde /usr/bin
 sudo cp meowtop-binary /usr/libexec/
 
-echo "Done! please logout and select Meowtop to complete installation"
-sleep 44
+echo "Running setup..."
+sudo chmod +x /usr/bin/Meowtop/Meowtop
+sudo chmod +x /usr/bin/meowtopde
+sudo chmod +x /usr/libexec/meowtop-binary
